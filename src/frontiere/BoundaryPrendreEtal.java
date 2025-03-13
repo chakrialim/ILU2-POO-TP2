22635533e3caf71ba21dedbd1057574f1dabe520
Chakria Lim <lmc5000a@ensfsi.onmicrosoft.com>
2025-03-13 20:00:32 +0100

    Fin TP2 pas fini

--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -10,10 +10,29 @@ public class BoundaryPrendreEtal {
 	}
 
 	public void prendreEtal(String nomVendeur) {
-		//TODO a completer
+		boolean nomVendeurConnu = controlPrendreEtal.verifierIdentite(nomVendeur);
+		if (!nomVendeurConnu) {
+			System.out.println("Je suis desolee " + nomVendeur
+					+ " mais il faut etre un habitant de notre village pour commencer ici");
+		} else {
+			System.out.println("Bonjour " + nomVendeur + " ,je vais regarder si je peux vous trouver un etal.");
+			boolean etalDisponible = controlPrendreEtal.resteEtals();
+			if (!etalDisponible) {
+				System.out.println("Desolee " + nomVendeur + " je n'ai plus d'etal qui soit pas deja occupe");
+			} else {
+				installerVendeur(nomVendeur);
+			}
+		}
 	}
 
 	private void installerVendeur(String nomVendeur) {
-		//TODO a completer
+		System.out.println("C'est parfait, il me reste un etal pour vous!");
+		System.out.println("Il me faudrait quelques renseignements: ");
+		String produit = Clavier.entrerChaine("Quel produit souhqitez-vous vendre?");
+		int nbProduit = Clavier.entrerEntier("Combien souhaitez-vous en vendre?");
+		controlPrendreEtal.prendreEtal(nomVendeur, produit, nbProduit);
+		int numeroEtal = controlPrendreEtal.prendreEtal(nomVendeur, produit, nbProduit);
+		if (numeroEtal!=-1) {
+			System.out.println("Le vendeur "+nomVendeur+" s'est installe a l'etal n°"+numeroEtal);
 	}
 }
